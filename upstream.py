b82c569d6a9f1f0f08b3cd8e33631c091d351d18
Akari <barryofc11@gmail.com>
2024-08-26 09:27:55 -0300

    upstream: use the branch fourteen_tests

--- upstream.py
+++ upstream.py
@@ -98,12 +98,11 @@ if __name__ == "__main__":
     upstream_repo = "https://github.com/openela/kernel-lts.git"
     upstream_branch = "linux-4.14.y"
     branches = [
-        "fourteen_dynamic_noksu",
-        "fourteen_dynamic",
-        "without-ksu",
-        "fourteen",
-#        "openela",
-#        "susfs"
+         "fourteen_tests"
+#        "fourteen_dynamic_noksu",
+#        "fourteen_dynamic",
+#        "without-ksu",
+#        "fourteen",
     ]
 
     #clone_repo()
